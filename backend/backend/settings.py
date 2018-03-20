e9439f5cd43b27c6d9b9aec14b985d92dc4934ae
guillermo lunar <maxinsideyou@gmail.com>
2018-03-20 08:55:49 -0700

    Added Dockerfile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static'),
-     os.path.join(BASE_DIR, 'bundle'),
+     os.path.join(BASE_DIR, 'build'),
 ]
 STATIC_ROOT = '/static/'
 
